72fddd34eb3100019275f4749e765fd2245db39d
gozooit <gozoo.it@protonmail.com>
2023-04-19 12:37:37 +0200

    add Board logic

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -49,4 +49,85 @@ module Mastermind
       str.each_char.all? { |char| COLORS.keys.map(&:to_s).include?(char) }
     end
   end
+
+  class Board
+    def initialize(secret_code = nil)
+      @secret_code = secret_code || Code.new.colors
+      @grid = []
+      @res = []
+      puts "secret code is #{@secret_code}"
+    end
+
+    def display
+      @grid.each_with_index do |line, index|
+        puts line.join(' - ')
+        print_result_to_symbols(@res[index])
+        puts
+      end
+    end
+
+    def add_guess(guessed_code)
+      @grid << guessed_code
+      @res << analyse_guessed_code(guessed_code)
+      @res.last
+    end
+
+    def full?
+      @grid.length == 12
+    end
+
+    def win?
+      @res.last.all? { |res| res == true }
+    end
+
+    def turn_elapsed
+      @grid.length
+    end
+
+    def analyse_guessed_code(guessed_code)
+      guessed_code, secret_code = remove_matches_from_codes(guessed_code.dup, @secret_code.dup)
+      find_misplaced_colors(guessed_code, secret_code)
+    end
+
+    private
+
+    def print_result_to_symbols(result)
+      result.each_with_index do |r, index|
+        print '✓' if r == true
+        print '𐄂' if r == false
+        print '?' if r == '?'
+        print index == 3 ? "\n" : ' - '
+      end
+    end
+
+    def guess_match_secret?(guessed_code)
+      result = []
+      4.times { |i| result << (guessed_code[i] == @secret_code[i]) }
+      result
+    end
+
+    def remove_matches_from_codes(guessed_code, secret_code)
+      matches = guess_match_secret?(guessed_code)
+      matches.each_with_index do |match, index|
+        if match
+          guessed_code[index] = match
+          secret_code[index] = match
+        end
+      end
+      [guessed_code, secret_code]
+    end
+
+    def find_misplaced_colors(guessed_code, secret_code)
+      guessed_code.map do |color|
+        next true if color == true
+
+        if secret_code.include?(color)
+          secret_code.delete_at(secret_code.index(color))
+          '?'
+        else
+          false
+        end
+      end
+    end
+  end
 end
